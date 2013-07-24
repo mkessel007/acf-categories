2c49913c65fd638537f7a4b887705b47fe0e274c
Cubeweb <info@cubeweb.gr>
2013-07-25 02:26:36 +0300

    Added check for null value Fixed chosen sellect all, sellect none functionality when multiple instances on page

--- acf-categories.php
+++ acf-categories.php
@@ -3,26 +3,11 @@
 	Plugin Name: Advanced Custom Fields: Categories
 	Plugin URI: https://github.com/cubeweb/acf-addons
 	Description: Categories is custom field that generates a multi drop down with all the categories or taxonomies from your wordpress site
-	Version: 2.0.0.1 Beta
+	Version: 2.0.0 Beta
 	Author: Cubeweb
 	Author URI: http://www.cubeweb.gr
 	License: GPLv2 or later
 	License URI: http://www.gnu.org/licenses/gpl-2.0.html
-	*/
-
-	/*
- 	This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 	*/
 
 
